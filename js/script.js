da41aeac634c8947d13bf7ec96623fb0a27b938d
hardeepsinhraj <hkraj0209@gmail.com>
2026-04-12 23:46:59 -0400

    Enable AR.

--- js/script.js
+++ js/script.js
@@ -4,8 +4,20 @@ const canvas = document.getElementById('renderCanvas');
 const engine = new BABYLON.Engine(canvas, true);
 //call the createScene function
 const createScene = async function () {
+    
 const scene = new BABYLON.Scene(engine);
 scene.clearColor = new BABYLON.Color3(0.8,0.9,1);
+
+// enable AR
+ const xr = await scene.createDefaultXRExperienceAsync({
+        uiOptions: {
+            sessionMode: "immersive-ar",
+           
+            referenceSpaceType: "local-floor"
+        },
+        
+        optionalFeatures: ["hit-test", "anchors"]
+    });
 
 /* Camera */
 const camera = new BABYLON.ArcRotateCamera("camera",-Math.PI / 2,Math.PI / 2.5,15,new BABYLON.Vector3(0, 0, 0), scene);
@@ -30,52 +42,74 @@ ground.material = groundMat;
 const table = BABYLON.MeshBuilder.CreateBox("table", {width: 4, height: 1.5, depth: 1}, scene);
 table.position = new BABYLON.Vector3(0, 0.75, 4);
 
+
+
 const tableMat = new BABYLON.StandardMaterial("tableMat", scene);
 tableMat.diffuseColor = new BABYLON.Color3(0.6,0.3,0.1);
 table.material = tableMat;
 
 /*cafe-Avatar */
 
-    const barista = BABYLON.SceneLoader.ImportMeshAsync("", "../meshes/", "barista3.glb").then((result) => {
-        // Do this stuff after the mesh has loaded
-        const baristaMesh = result.meshes[0];
-        // Grab the bouding box
-        const baristaBounds = result.meshes[1];
-        baristaBounds.showBoundingBox = true;
-        // wheelMesh.position = new BABYLON.Vector3(0, 0, 0);
-        baristaMesh.scaling = new BABYLON.Vector3(50, 50, 70);
-        baristaMesh.position = new BABYLON.Vector3(0.3, 0.9, 4.9);
-    }).catch((error) => {
-        // Oops, the mesh didn't load for some reason
-        console.error("Error loading mesh: " + error);
-        return null;
+    // BARISTA
+let baristaMeshes;
+
+await BABYLON.SceneLoader.ImportMeshAsync("", "../meshes/", "barista3.glb", scene)
+.then((result) => {
+
+    baristaMeshes = result.meshes;
+    const rootMesh = result.meshes[0];
+    //rotate from back to froent
+    rootMesh.rotation.y += Math.PI;
+
+    baristaMeshes.forEach(mesh => {
+
+        if (!mesh.getTotalVertices || mesh.getTotalVertices() === 0) return;
+
+        mesh.scaling = new BABYLON.Vector3(50, 50, 70);
+        mesh.position = new BABYLON.Vector3(0.3, -4.7, 0.5);
+        mesh.rotation.y += Math.PI;
+        mesh.isPickable = true;
     });
-
-
-
-const waiter = BABYLON.SceneLoader.ImportMeshAsync("", "../meshes/", "waiter.glb").then((result) => {
-    // Do this stuff after the mesh has loaded
-    const waiterMesh = result.meshes[0];
-    waiterMesh.scaling = new BABYLON.Vector3(50, 50, 70);
-    waiterMesh.position = new BABYLON.Vector3(1.5, 0, 0.2);
-}).catch((error) => {
-    // Oops, the mesh didn't load for some reason
-    console.error("Error loading mesh: " + error);
-    return null;
 });
 
 
-const customer = BABYLON.SceneLoader.ImportMeshAsync("", "../meshes/", "customer1.glb").then((result) => {
-    // Do this stuff after the mesh has loaded
-    const customerMesh = result.meshes[0];
-    customerMesh.scaling = new BABYLON.Vector3(50, 50, 70);
-    customerMesh.position = new BABYLON.Vector3(-3, 0.5, -0.1);
-}).catch((error) => {
-    // Oops, the mesh didn't load for some reason
-    console.error("Error loading mesh: " + error);
-    return null;
+
+
+// WAITER
+let waiterMeshes;
+
+await BABYLON.SceneLoader.ImportMeshAsync("", "../meshes/", "waiter.glb", scene)
+.then((result) => {
+
+    waiterMeshes = result.meshes;
+
+    waiterMeshes.forEach(mesh => {
+
+        if (!mesh.getTotalVertices || mesh.getTotalVertices() === 0) return;
+
+        mesh.scaling = new BABYLON.Vector3(50, 50, 70);
+        mesh.position = new BABYLON.Vector3(-1.5, 0, 0.2);
+        mesh.isPickable = true;
+    });
 });
 
+// CUSTOMER
+let customerMeshes;
+
+await BABYLON.SceneLoader.ImportMeshAsync("", "../meshes/", "customer1.glb", scene)
+.then((result) => {
+
+    customerMeshes = result.meshes;
+
+    customerMeshes.forEach(mesh => {
+
+        if (!mesh.getTotalVertices || mesh.getTotalVertices() === 0) return;
+
+        mesh.scaling = new BABYLON.Vector3(50, 50, 70);
+        mesh.position = new BABYLON.Vector3(3, 0, 0.3);
+        mesh.isPickable = true;
+    });
+});
 
 /*Chair */
 const chair = BABYLON.MeshBuilder.CreateBox("chair", {width: 1, height: 1, depth: 1}, scene);
@@ -98,39 +132,48 @@ text.top = "-40%";
 gui.addControl(text);
 
 /* Interaction */
-barista.actionManager = new BABYLON.ActionManager(scene);
 
-barista.actionManager.registerAction(
-new BABYLON.ExecuteCodeAction(
-BABYLON.ActionManager.OnPickTrigger,
-function(){
-text.text = "Hello! What would you like to order?";
-}
-)
-);
+baristaMeshes.forEach(mesh => {
 
-waiter.actionManager = new BABYLON.ActionManager(scene);
+    mesh.actionManager = new BABYLON.ActionManager(scene);
 
-waiter.actionManager.registerAction(
-new BABYLON.ExecuteCodeAction(
-BABYLON.ActionManager.OnPickTrigger,
-function(){
-text.text = "Would you like coffee or tea?";
-}
-)
-);
+    mesh.actionManager.registerAction(
+        new BABYLON.ExecuteCodeAction(
+            BABYLON.ActionManager.OnPickTrigger,
+            function(){
+                text.text = "Hello! What would you like to order?";
+            }
+        )
+    );
+});
 
-customer.actionManager = new BABYLON.ActionManager(scene);
+waiterMeshes.forEach(mesh => {
 
-customer.actionManager.registerAction(
-new BABYLON.ExecuteCodeAction(
-BABYLON.ActionManager.OnPickTrigger,
-function(){
-text.text = "Is this seat taken?";
-}
-)
-);
+    mesh.actionManager = new BABYLON.ActionManager(scene);
 
+    mesh.actionManager.registerAction(
+        new BABYLON.ExecuteCodeAction(
+            BABYLON.ActionManager.OnPickTrigger,
+            function(){
+                text.text = "Would you like coffee or tea?";
+            }
+        )
+    );
+});
+
+customerMeshes.forEach(mesh => {
+
+    mesh.actionManager = new BABYLON.ActionManager(scene);
+
+    mesh.actionManager.registerAction(
+        new BABYLON.ExecuteCodeAction(
+            BABYLON.ActionManager.OnPickTrigger,
+            function(){
+                text.text = "Is this seat taken?";
+            }
+        )
+    );
+});
 
 
 return scene;
